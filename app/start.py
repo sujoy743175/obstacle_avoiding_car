325204a9c14e9df1048506640f20ceace81daf7b
sujoy743175 <sujoy743175@yahoo.co.in>
2021-08-24 22:47:10 +0530

    start.py modified

--- app/start.py
+++ app/start.py
@@ -1,10 +1,11 @@
 from app.obstacleavoidance import avoid
 from app.blink import blink
-from app.blink import ble
+from app.ble import ble
 
 def start():
     print('now its ble..')
     #blink()
-    #avoid()    
+    #avoid() 
+    ble()   
 
 
