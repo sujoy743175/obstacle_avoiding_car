3a01ffb9f0a7548f54dbc34494e9fe77d07fa0cf
sujoy <sujoy743175@yahoo.co.in>
2021-08-24 20:34:08 +0530

    ble added

--- app/start.py
+++ app/start.py
@@ -1,9 +1,10 @@
 from app.obstacleavoidance import avoid
 from app.blink import blink
+from app.blink import ble
 
 def start():
-    print('now its working..')
-    blink()
+    print('now its ble..')
+    #blink()
     #avoid()    
 
 
