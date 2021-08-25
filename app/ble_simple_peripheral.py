9211f2d2f27d8b6d8d31c5f501104c4b54aabc8e
sujoy743175 <sujoy743175@yahoo.co.in>
2021-08-25 21:58:50 +0530

    added new file repl

--- app/ble_simple_peripheral.py
+++ app/ble_simple_peripheral.py
@@ -4,7 +4,7 @@ import bluetooth
 import random
 import struct
 import time
-from ble_advertising import advertising_payload
+from app.ble_advertising import advertising_payload
 
 from micropython import const
 
--- app/start.py
+++ app/start.py
@@ -1,11 +1,13 @@
-from app.obstacleavoidance import avoid
-from app.blink import blink
+#from app.obstacleavoidance import avoid
+#from app.blink import blink
 #from app.ble import ble
-from app.ble_simple_peripheral import demo
+#from app.ble_simple_peripheral import demo
+from app.ble_uart_repl import start
 
 def start():
     #print('now its ble..')
     #blink()
     #avoid()
     #ble()
-    demo()
+    #demo()
+    start()
