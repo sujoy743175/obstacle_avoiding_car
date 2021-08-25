2cb03a050550fdd7a0664c03a0e4502edc7450f3
sujoy743175 <sujoy743175@yahoo.co.in>
2021-08-25 19:31:23 +0530

    added new file

--- app/start.py
+++ app/start.py
@@ -1,11 +1,11 @@
 from app.obstacleavoidance import avoid
 from app.blink import blink
-from app.ble import ble
+#from app.ble import ble
+from app.ble_simple_central import demo
 
 def start():
     #print('now its ble..')
     #blink()
-    #avoid() 
-    ble()   
-
-
+    #avoid()
+    #ble()
+    demo()
