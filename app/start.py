115dee59b7260b6bee507e1096404e18cad1a006
sujoy743175 <sujoy743175@yahoo.co.in>
2021-08-25 23:08:40 +0530

    start modified with repl success

--- app/start.py
+++ app/start.py
@@ -2,12 +2,13 @@
 #from app.blink import blink
 #from app.ble import ble
 #from app.ble_simple_peripheral import demo
-from app.ble_uart_repl import start_repl
+#from app.ble_uart_repl import start_repl
+from app.ble_uart_peripheral import demo
 
 def start():
     #print('now its ble..')
     #blink()
     #avoid()
     #ble()
-    #demo()
-    start_repl()
+    demo()
+    #start_repl()
