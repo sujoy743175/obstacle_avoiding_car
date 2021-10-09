6afd8c706afccb1734c8aae42562ddc958f65332
sujoy743175 <sujoy743175@yahoo.co.in>
2021-10-09 10:43:09 +0530

    ultrasound sensor added

--- app/obstacleavoidance.py
+++ app/obstacleavoidance.py
@@ -1,15 +1,19 @@
 from app.test import *
 from app.HCSR_04 import HCSR04
+from utime import sleep
 
-sensor = HCSR04(23, 24)
+sensor = HCSR04(32, 35)
 
 def avoid():
     forward_distance = sensor.distance_cm()
-    if forward_distance >= 10:
+    '''if forward_distance >= 10:
         forward()
 
     else:
         backward()
-        turnRihgt()
+        turnRihgt()'''
+    print(forward_distance)
+    sleep(2)
+
 
 
