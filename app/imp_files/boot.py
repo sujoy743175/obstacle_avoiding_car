842dbee3ddb8107e870ee5a10d5de4e02543d4cd
sujoy743175 <sujoy743175@yahoo.co.in>
2021-10-23 18:22:44 +0530

    webrepl started

---
+++ app/imp_files/boot.py
@@ -0,0 +1,21 @@
+def do_connect(ssid, pwd):
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(ssid, pwd)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+ 
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+ 
+# Attempt to connect to WiFi network
+do_connect('Investigator', 'sankalp@2015')
+ 
+import webrepl
+webrepl.stop()
+webrepl.start()
--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from app.start import *
 from app.connect_and_update import connectToWifiAndUpdate
 
 connectToWifiAndUpdate()
+webrepl.start(password='')
 
 while True:
     start()
     
+
+
 
 
